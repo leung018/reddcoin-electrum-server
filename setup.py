5134330d472feafa3e87ede016290191d1699ebe
laudney <ren@reddcoin.com>
2014-09-12 16:51:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ from setuptools import setup
 setup(
     name="electrum-server",
     version="0.9",
-    scripts=['run_electrum_server','electrum-server'],
-    install_requires=['plyvel','jsonrpclib', 'irc'],
-    package_dir={
-        'electrumserver':'src'
-        },
+    scripts=['run_electrum_server', 'electrum-server'],
+    install_requires=['plyvel', 'jsonrpclib', 'irc'],
+    package_dir={'electrumserver': 'src'},
     py_modules=[
         'electrumserver.__init__',
         'electrumserver.utils',
@@ -23,11 +21,9 @@ setup(
         'electrumserver.stratum_http'
     ],
     description="Reddcoin Electrum Server",
-    author="Thomas Voegtlin, Reddcoin Developers",
-    author_email="thomasv1@gmx.de",
-    license="GNU Affero GPLv3",
-    url="https://github.com/reddcoin-project/reddcoin-electrum-server/",
-    long_description="""Server for the Electrum Lightweight Reddcoin Wallet"""
+    author="Thomas Voegtlin, Larry Ren",
+    author_email="thomasv1@gmx.de, ren@reddcoin.com",
+    license="GNU GPLv3",
+    url="https://reddwallet.org",
+    long_description="""Reddcoin Electrum Server"""
 )
-
-
