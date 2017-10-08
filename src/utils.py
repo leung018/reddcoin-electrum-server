eb7390c398aeba643f15e08e8597dc4b3d1d2aaf
John Nash <redd@redd.ink>
2017-10-08 12:51:19 +1100

    Revert Fix double line logging in logfile

--- src/utils.py
+++ src/utils.py
@@ -241,7 +241,7 @@ def timestr():
 import logging
 import logging.handlers
 
-#logging.basicConfig(format="%(asctime)-11s %(message)s", datefmt="[%d/%m/%Y-%H:%M:%S]")
+logging.basicConfig(format="%(asctime)-11s %(message)s", datefmt="[%d/%m/%Y-%H:%M:%S]")
 logger = logging.getLogger('electrum')
 
 def init_logger(logfile):
